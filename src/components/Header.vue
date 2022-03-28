a755fc60998ae5f4876364db1da192e90af6cecc
giagra91 <giagra91@gmail.com>
2022-03-28 15:22:02 +0200

    completamento struttura header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,14 @@
 <template>
   <div class="home-header">
     <div id="header-container">
-    <h1>Header</h1>
-    <h1>Header</h1>
+      <img src="../assets/images/dc-logo.png" alt="DC logo">
+      <nav >
+        <ul>
+          <li v-for="(link,index) in links" :key="index">
+            <a :href="link.url" :class="{active: link.visible}">{{link.text}}</a>
+          </li>
+        </ul>
+      </nav>
     </div>
   </div>
 </template>
@@ -12,6 +18,58 @@ export default {
   name: 'HomeHeader',
   data: function(){
     return {
+      links: [
+        {
+          text: `Characters`,
+          url: `#`,
+          visible: true
+        },
+        {
+          text: `Comics`,
+          url: `#`,
+          visible: false
+        },
+        {
+          text: `Movies`,
+          url: `#`,
+          visible: false
+        },
+        {
+          text: `Tv`,
+          url: `#`,
+          visible: false
+        },
+        {
+          text: `Games`,
+          url: `#`,
+          visible: false
+        },
+        {
+          text: `Collectibles`,
+          url: `#`,
+          visible: false
+        },
+        {
+          text: `Videos`,
+          url: `#`,
+          visible: false
+        },
+        {
+          text: `Fans`,
+          url: `#`,
+          visible: false
+        },
+        {
+          text: `News`,
+          url: `#`,
+          visible: false
+        },
+        {
+          text: `Shop`,
+          url: `#`,
+          visible: false
+        },
+      ]
 
     }
   }
@@ -23,10 +81,50 @@ export default {
 <style lang="scss" scoped>
 div.home-header{
   width: 80%;
-  height: 60px;
-  background-color: blue;
+  height: 100px;
   margin: 0 auto;
 
+  div#header-container{
+    height: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 1rem;
 
+    img{
+      height: 70%;
+    }
+
+    nav{
+      height: 100%;
+      line-height: 100px;
+
+      ul{
+      height: 100%;
+      list-style-type: none;
+      display: flex;
+
+        li{
+          height: 100%;
+          padding: 0 .3rem;
+          text-transform: uppercase;
+          font-size: .8rem;
+
+          &:hover{
+            border-bottom: 2px solid #0c7cec;
+          }
+          a{
+            text-decoration: none;
+            font-weight: bold;
+            color: black;
+              &.active{
+                            color: #0c7cec;
+              }
+          }
+        }
+    }
+    }
+
+  }
 }
 </style>
